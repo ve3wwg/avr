e9b9ce55fd7e4ea877e8e49e82d61b3f395711c0
Warren Gay <ve3wwg@gmail.com>
2013-08-28 19:24:38 -0400

    Import

---
+++ ada/midi_lib/midi_test.adb
@@ -0,0 +1,11 @@
+with Interfaces;           use Interfaces;
+with MIDI.Receiver;
+with MIDI.Transmitter;
+
+procedure MIDI_Test is
+    IO :    MIDI.IO_Context;
+begin
+
+    MIDI.Initialize(IO);        -- This is missing I/O routines (don't run)
+
+end MIDI_Test;
