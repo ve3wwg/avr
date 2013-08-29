951b757287bd0c802b117e85ac07295e1092d314
Warren Gay <ve3wwg@gmail.com>
2013-08-28 23:24:36 -0400

    Cleanup

--- ada/midi_lib/midi_test.adb
+++ ada/midi_lib/midi_test.adb
@@ -4,9 +4,6 @@ with MIDI.Receiver;
 with MIDI.Transmitter;
 
 procedure MIDI_Test is
-    IO :    MIDI.IO_Context;
 begin
-
-    MIDI.Initialize(IO);        -- This is missing I/O routines (don't run)
-
+    null;
 end MIDI_Test;
