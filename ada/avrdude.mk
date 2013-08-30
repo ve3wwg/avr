b3fbd5523e5e63b64f7c38e2c71201f6e4cddc41
Warren Gay <ve3wwg@gmail.com>
2013-08-29 22:01:31 -0400

    Configure avrdude

---
+++ ada/avrdude.mk
@@ -0,0 +1,12 @@
+# avrdude parameters for programming:
+
+AVRDUDE_CONF=/opt/local/etc/avrdude.conf
+
+#PGMR	= avrisp
+PGMR	= arduino
+PGPORT	= /dev/cu.usbserial-A6005XBY
+PGBAUD	= 19200
+
+PGMPARMS= -C $(AVRDUDE_CONF) -c $(PGMR) -P $(PGPORT) -b $(PGBAUD)
+
+# End
