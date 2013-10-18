7ec1546c7336d7f1d421ce917fb898e22bc14ef0
Warren Gay <ve3wwg@gmail.com>
2013-10-17 21:44:58 -0400

    Now using UNO Bootloader in ATmega328p devices

--- ada/avrdude.mk
+++ ada/avrdude.mk
@@ -5,7 +5,8 @@ AVRDUDE_CONF=/opt/local/etc/avrdude.conf
 #PGMR	= avrisp
 PGMR	= arduino
 PGPORT	= /dev/cu.usbserial-A6005XBY
-PGBAUD	= 19200
+#PGBAUD	= 19200
+PGBAUD	= 115200
 
 PGMPARMS= -C $(AVRDUDE_CONF) -c $(PGMR) -P $(PGPORT) -b $(PGBAUD)
 
