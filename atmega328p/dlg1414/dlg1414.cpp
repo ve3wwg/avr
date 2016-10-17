af642395d5a1a5de4d4817ac1896ef3d662de932
Warren Gay <ve3wwg@gmail.com>
2016-10-16 23:57:49 -0400

    Minor cleanup

--- atmega328p/dlg1414/dlg1414.cpp
+++ atmega328p/dlg1414/dlg1414.cpp
@@ -4,18 +4,35 @@
 // License: GPLv3
 ///////////////////////////////////////////////////////////////////////
 //
+//  PCB requires one modification (see Notes).
+//
 //  This source code drives a reverse engineered PCB from some
 //  ancient piece of equipment, from a far away galaxy.. The number
 //  on the PCB was PC1031-0B (bottom) and PC1031-OT (topside).
 //
 //  The PCB housed 8 x DLG-1414 modules, each with 4 characters of
 //  display. You could also use DLR-1414 or DLO-1414 modules instead,
-//  if you like red or bright red. The DLG-1414 are green.
+//  if you like red or bright red. The DLG-1414 modules are green.
 //
-//  The pcb uses 2 x CD4094 shift registers, 1 x CD4001 and one
+//  The pcb uses 3 x CD4094 shift registers, 1 x CD4001 and one
 //  CD4021 to read the optional pushbuttons. The shift register
 //  allows use of a 24-bit command word to control the display
 //  modules.
+//
+//  PCB CONNECTIONS
+//
+//  Looking at the back of the PCB, with the connections appearing
+//  at the upper right hand corner:
+//  
+//      --------------------------+
+//          +5V Supply..........O |
+//          Push Button Data....O |
+//          Command Data........O |
+//          Strobe (STR)........O |
+//          Clock Pulse (CP)....O |
+//          Ground..............O |
+//       pcb (bottom view)        |
+//                                |
 //
 //  24-BIT COMMAND STRUCTURE:
 //
@@ -39,21 +56,18 @@
 //      21      /WR 5
 //      22      /WR 6
 //      23      /WR 7
+//     (24)     D6      From IC3.Q7S output
 //    
 //  NOTES:
-//      1.  D6 is delivered through the pcb hack:
-//          The last bit is clocked in with a clock
-//          pulse (CP) like all bits except that:
+//      1.  D6 is delivered through Q7S of IC3 (CD4094).
+//          The pcb must be modified as follows:
 //  
-//          If CP is left high when STR is asserted,
-//          then D6=0 until the STR is returned low.
+//          i)  Remove IC4 (CD4001), it is not required
+//          ii) Solder a wire from IC4's pin 3 to IC3's
+//              pin 10 (Q7S).
 //
-//          If CP is LOW when STR is asserted, then
-//          pin 2 of CD4001 is 2.5 volts, by means
-//          of voltage divider resistors. This level is
-//          lower than the VIH=3 to 3.5 volts of CD4001.
-//          Since it is a NOR gate, the output to D6 is
-//          inverted as D6=1.
+//          This modification permits display of the
+//          entire DLG-1414 character set.
 //
 //	2.  This code assumes that only PORTD will be
 //          used to interface with the pcb. Macros CP,
@@ -90,25 +104,25 @@ class DLG_1414 {
 	union	{
 		uint32_t	cmd;	// 24-bit command word
 		struct	{
-			unsigned	wr03 : 4;	// /WR0..3 (LSB)
-			unsigned	led03 : 4;	// /LED0..3
-			unsigned	d3 : 1;		// D3
-			unsigned	d2 : 1;		// D2
-			unsigned	d1 : 1;
-			unsigned	d0 : 1;
-			unsigned	led47 : 4;	// /LED4..7
-			unsigned	d5 : 1;		// D5
-			unsigned	d4 : 1;		// D4
-			unsigned	a1 : 1;		// A1
-			unsigned	a0 : 1;		// A0
-			unsigned	wr47 : 4;	// /WR4..7
-			unsigned	d6 : 1;		// D6
-			unsigned	fill : 7;	// MSB
+			uint32_t	wr03 : 4;	// /WR0..3 (LSB)
+			uint32_t	led03 : 4;	// /LED0..3
+			uint32_t	d3 : 1;		// D3
+			uint32_t	d2 : 1;		// D2
+			uint32_t	d1 : 1;
+			uint32_t	d0 : 1;
+			uint32_t	led47 : 4;	// /LED4..7
+			uint32_t	d5 : 1;		// D5
+			uint32_t	d4 : 1;		// D4
+			uint32_t	a1 : 1;		// A1
+			uint32_t	a0 : 1;		// A0
+			uint32_t	wr47 : 4;	// /WR4..7
+			uint32_t	d6 : 1;		// D6
+			uint32_t	fill : 7;	// MSB
 		} sr;
 	} u;
 	unsigned	leds;		// Saved LED settings
 	union 	{
-		uint8_t	indata;
+		unsigned indata;
 		struct { 		// Input order
 			unsigned pb4 : 1;
 			unsigned pb3 : 1;
@@ -118,32 +132,22 @@ class DLG_1414 {
 			unsigned pb6 : 1;
 			unsigned pb5 : 1;
 			unsigned pb0 : 1;
+			unsigned fill : 8;
 		} sr;
 	} pb;
 	unsigned	pbdata;		// Input push button data
 
 protected:
+	void setup();				// Internal
 	void write_cmd(uint32_t cmd);           // Internal
 
 public:	DLG_1414();
-	void writec(char c,unsigned digit);	// Write character
-	unsigned read();			// Read pushbuttons
+	void writec(unsigned char c,unsigned digit);	// Write character
+	unsigned read(bool strobe=true);	// Read pushbuttons
 	bool set_led(bool b,unsigned led);	// Set one LED
 	unsigned get_buttons() { return pbdata; }
 };
 	
-//////////////////////////////////////////////////////////////////////
-// Internal signal output function
-//////////////////////////////////////////////////////////////////////
-
-static void
-put_sigs(uint8_t s) {
-	static const uint8_t mask = _BV(CP)|_BV(STR)|_BV(D);
-	uint8_t temp = PORTD & ~mask;
-
-	PORTD = temp | (s & mask);
-}
-
 //////////////////////////////////////////////////////////////////////
 // DLG_1414 Constructor
 //////////////////////////////////////////////////////////////////////
@@ -151,10 +155,25 @@ put_sigs(uint8_t s) {
 DLG_1414::DLG_1414() {
 	DDRD |= _BV(CP)|_BV(STR)|_BV(D); // outputs
 	DDRD &= ~_BV(PBD);		 // input
-	put_sigs(0);			 // clear line state
+        PORTD &= ~(_BV(CP)|_BV(STR)|_BV(D));
 	leds = 0;
 	pbdata = 0;
 };
+
+//////////////////////////////////////////////////////////////////////
+// Protected: Initialize command word
+//////////////////////////////////////////////////////////////////////
+
+void
+DLG_1414::setup() {
+
+	u.cmd = 0;
+	u.sr.wr03 = u.sr.wr47 = 0x0F;	// all /WR=1
+
+	// Maintain existing LED status
+	u.sr.led03 = (leds & 0x0F) ^ 0x0F;
+	u.sr.led47 = ((leds >> 4) & 0x0F) ^ 0x0F;
+}
 
 //////////////////////////////////////////////////////////////////////
 // Internal: Write a 24-bit command to display driver pcb
@@ -165,22 +184,28 @@ DLG_1414::write_cmd(uint32_t cmd) {
 	uint32_t sr = cmd;
 	uint8_t d;
 
+	PORTD &= ~(_BV(CP)|_BV(STR)|_BV(D));
+
+	// Send D6 first, so that at the end it is in IC3's Q7S output
+
+	if ( (cmd & 0x1000000) != 0 )	 	// D6?
+		PORTD |= _BV(D);		// D=1 (D6)
+	PORTD |= _BV(CP);			// CP=1
+	PORTD &= ~_BV(CP);			// CP=0
+
 	for ( int x=0; x<24; ++x ) {
 		d = (sr & (uint32_t(1) << 23)) ? 1 : 0;
 		sr <<= 1;
-		put_sigs(d ? _BV(D) : 0);	// D first, CP=0
-		put_sigs((d ? _BV(D) : 0)|_BV(CP)); // Raise CP=1
+		PORTD &= ~_BV(CP);		// CP=0
+		if ( d )
+			PORTD |= _BV(D);
+		else	PORTD &= ~_BV(D);
+		PORTD |= _BV(CP);		// CP=1
 	}
 
-	if ( (cmd & (uint32_t(1) << 24)) != 0 ) { // D6?
-		put_sigs(_BV(STR));		// CP=0 & STR=1 => D6=1
-	} else	{
-		put_sigs(_BV(CP)|_BV(STR));	// CP=1 & STR=1 => D6 = 0
-	}
-
-	put_sigs(0);				// CP=0, STR=0
-	read();
-//	leds = pbdata;		// Leave this in for push button demo
+	PORTD |= _BV(STR);			// STR=1
+	PORTD &= ~_BV(STR);			// STR=0
+	PORTD &= ~_BV(CP);			// CP=0 (this also sets Q7S=QP7 for IC3)
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -188,14 +213,19 @@ DLG_1414::write_cmd(uint32_t cmd) {
 //////////////////////////////////////////////////////////////////////
 
 unsigned
-DLG_1414::read() {
+DLG_1414::read(bool strobe) {
 	int x;
 
-	pbdata = 0;
+	if ( strobe ) {
+		setup();			// Reset cmd word
+		write_cmd(u.cmd);
+	}
+
+	pb.indata = 0;
 	for ( x=0; x<8; ++x ) {
-		pb.indata = (pb.indata << 1) | (!(PIND & _BV(PBD)));
-		put_sigs(_BV(CP));
-		put_sigs(0);
+		pb.indata = (pb.indata << 1) | !(PIND & _BV(PBD));
+		PORTD |= _BV(CP);
+		PORTD &= ~_BV(CP);
 	}
 
 	// Unscramble this willy nilly mess
@@ -215,7 +245,7 @@ DLG_1414::read() {
 //////////////////////////////////////////////////////////////////////
 
 void
-DLG_1414::writec(char c,unsigned digit) {
+DLG_1414::writec(unsigned char c,unsigned digit) {
 	uint8_t uc = uint8_t(c);	// Unsigned char
 	unsigned m = digit >> 2;	// Module select 0..7
 	uint8_t d = (uint8_t(digit) & 3u) ^ 3; // Digit 0..3
@@ -225,8 +255,7 @@ DLG_1414::writec(char c,unsigned digit) {
 		msel = (1 << m);	// First group
 	else 	msel = (uint32_t(1) << uint32_t(m-4)) << 20;
 
-	u.cmd = 0;
-	u.sr.wr03 = u.sr.wr47 = 0x0F;	// all /WR=1
+	setup();			// Initialize cmd word
 	u.sr.a0 = !!(d & 1);		// A0
 	u.sr.a1 = !!(d & 2);		// A1
 	u.sr.d0 = !!(uc & 0x01);	// D0 ..
@@ -237,13 +266,10 @@ DLG_1414::writec(char c,unsigned digit) {
 	u.sr.d5 = !!(uc & 0x20);
 	u.sr.d6 = !!(uc & 0x40);	// D6
 
-	// Maintain existing LED status
-	u.sr.led03 = (leds & 0x0F) ^ 0x0F;
-	u.sr.led47 = (leds >> 4) ^ 0x0F;
-
 	write_cmd(u.cmd);		// /WR=1, stabilize A1,A0
 	write_cmd(u.cmd^msel);		// /WR=0
 	write_cmd(u.cmd);		// /WR=1
+	read(false);
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -260,10 +286,7 @@ DLG_1414::set_led(bool b,unsigned led) {
 	else	leds &= ~mask;
 
 	if ( leds != before ) {
-		u.cmd = 0;
-		u.sr.wr03 = u.sr.wr47 = 0x0F;	// all /WR=1
-		u.sr.led03 = (leds & 0x0F) ^ 0x0F;
-		u.sr.led47 = (leds >> 4) ^ 0x0F;
+		setup();
 		write_cmd(u.cmd);
 	}
 	return !!(leds & mask);
@@ -276,17 +299,41 @@ DLG_1414::set_led(bool b,unsigned led) {
 int
 main() {
 	DLG_1414 dlg1414;	// Instantiated class
-	char c;			// Temp. Character to display
+	unsigned char c;	// Temp. Character to display
 	unsigned dx;		// Temp. Digit index
+	unsigned pb;		// Temp. Push button data
+
+#if 0
+	for (;;) {
+		for ( c = 0x00; c<0x80; ) {
+			// Display 8 modules (8x4 characters)
+			for ( dx=0; dx<32; ++dx ) {
+				dlg1414.writec(c,dx);
+				++c;
+			}
+			_delay_ms(800);
+		}
+		_delay_ms(800);
+	}
+#endif
+
+	c = 'A';
+	for ( dx=0; dx<32; ++dx ) {
+		dlg1414.writec(c,dx);
+		++c;
+	}
 
 	for (;;) {
-		c = '0';
-		// Display 8 modules (8x4 characters)
-		for ( dx=0; dx<32; ++dx ) {
-			dlg1414.writec(c,dx);
-			++c;
+		pb = dlg1414.read();	// Read pb
+		for ( int x=0; x<8; ++x ) {
+			c = 'A'+x;
+			if ( pb & 1 )
+				c = 'a'+x;
+			dlg1414.writec(c,x);
+			dlg1414.set_led(pb&1,x);
+			pb >>= 1;
 		}
-		_delay_ms(600);
+//		_delay_ms(600);
 	}
 
 	return 0;
